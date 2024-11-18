e3294997f482f0e5ec0389c57f4bb0f4d83ec777
Stupiedidiot <anothernoobeyneko@gmail.com>
2024-11-18 22:05:47 +0800

    modified blog

--- public/blog/main.js
+++ public/blog/main.js
@@ -1,78 +1,180 @@
-masterlist = [
-    {"file":"2024-06-15-Banana-Overlord.html","alt":null,"tags":["comics","pictures"]},
-    {"file":"2023-08-29-Pakka-Pets.html","alt":null,"tags":["games","pictures"]},
-    {"file":"2023-08-28-Hotdog.html","alt":null,"tags":["comics","pictures"]},
-    {"file":"2023-08-20-Meet-the-Fellas.html","alt":null,"tags":["pictures"]},
-    {"file":"2023-08-19-Minecraft.html","alt":"Minecraft irl!?!?!?","tags":["pictures"]}
+// Tabvoid by Stupied: https://stupied.neocities.org
+
+//TABLE OF CONTENTS
+  // 1. User Info 
+  // 2. Post List
+  // 3. Settings
+  // 4. Da Code!
+  // 5. Functions
+
+//-----------------------------
+
+// [1] USER INFO
+const blogname = "The Abyss"
+const username = "Stupied"
+const userlink = "https://stupied.neocities.org/about"
+
+//-----------------------------
+
+// [2] POST LIST 
+
+const posts=[
+	{"file":"2024-06-15-Banana-Overlord.html"},
+    {"file":"2023-08-29-Pakka-Pets.html"},
+    {"file":"2023-08-28-Hotdog.html"},
+    {"file":"2023-08-20-Meet-the-Fellas.html"},
+    {"file":"2023-08-19-Minecraft.html","alt":"Minecraft irl!?!?!?"}
 ]
 
-const url = window.location.pathname;
-relativePath = "./";
-urlPath = url.split('/');
-if(urlPath.length > 2){for(i=0;i<urlPath.length-2;i++){relativePath+="../"}}
+//-----------------------------
 
-function addTo(item,input){
-    if (item) {
-        if(typeof input=="string"){
-            item.innerHTML+=input;
-        } else {
-            item.append(input);
-        }
-    }
+// [3] SETTINGS - The page's format!
+
+const url = window.location.pathname;relativePath = "./";
+//if(url.includes("post")){relativePath="./../";}
+// add "relativePath" to any anchor tags or file paths
+
+const header='\
+'
+
+// <img src="' + relativePath + 'img/banner.png">\
+
+const navigation ='\
+<div>\
+    <a href="#"><img src="' + relativePath + '../meta/pfp.png"></a>\
+    <h3>'+ blogname +'</h3>\
+	<nav>\
+		<a href="' + relativePath + 'index.html">Home</a>\
+		<a href="' + relativePath + '../about.html">About</a>\
+		<a href="' + relativePath + 'archive.html">Archive</a>\
+		<a href="https://status.cafe/users/stupied">Microblog</a>\
+		<a href="' + relativePath + '../index.html">Head Back</a>\
+	</nav>\
+</div>\
+'
+
+const footer ='\
+<p>\
+'+blogname+' is written by <a href="'+userlink+'">'+username+'</a>. \
+Created with <a href="https://stupied.neocities.org/blog/tabvoid/">Tabvoid</a>\
+</p>\
+'
+
+const template ='\
+<div id="container">\
+	<header>'+ header +'</header>\
+	<div id="nav">'+ navigation +'</div>\
+	<main></main>\
+	<footer>'+ footer +'</footer>\
+</div>\
+\
+'
+
+//-----------------------------
+
+// [4] DA CODE - You don't have to touch anything here (unless you really wanna)
+
+
+document.querySelector("body").innerHTML+= template;
+
+main=document.querySelectorAll(".main-content");
+for(i=0; i<main.length; i++){
+    document.querySelector("#container main").append(main[i]);
 }
 
-addToTag("head", '<link rel="icon" type="image/x-icon" href="'+relativePath+'meta/favicon.ico"></link>');
-addToId("comments",' <div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Beep Boop</a>, hold please!</div><link rel="stylesheet" type="text/css" href="https://www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" /><style>#HCB_comment_box img{width:auto;display:inline-block;}.home-desc{display:none;}#HCB_comment_box h3:first-child{margin:0;}</style>');
+if(e=document.querySelector("header")){e.innerHTML=(header);}
+if(e=document.querySelector("#nav")){e.innerHTML=(navigation);}
+if(e=document.querySelector("footer")){e.innerHTML=(footer);}
 
-window.onload = function(){
-    if(document.getElementById("comments")){
-        if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24lorU9Glfj8bQyg9yk9caG%2F"+"&opts=16798&num=10&ts=1699153972795");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})();
-    }
+document.querySelector("head").innerHTML+='<link rel="icon" type="image/x-icon" href="'+relativePath+'style/favicon.ico"></link>'
+
+currentIndex = getIndex(posts)
+if(currentIndex>-1){
+	currentTitle = getTitle(currentIndex,posts)
+	if(document.title===""){document.title=currentTitle;}
+	
+	currentTitle = "<h1>" + currentTitle + "</h1>"
+	
+	currentDate =  getDate(currentIndex,posts)
+	currentDate = convDate(currentDate) // converts to word
+	currentDate =  "<h4>" + currentDate + "</h4>"
+
+	currentNav = genNav(posts)
+
+	if(e=document.querySelector("#blog-title")){e.innerHTML=(currentTitle);}
+	if(e=document.querySelector("#blog-date")){e.innerHTML=(currentDate);}
+	if(e=document.querySelector("#nextprev")){e.innerHTML=(currentNav);}
+} else {
+	if(e=document.querySelector("#blog-title")){e.innerHTML="[Unlisted Post]";}
+
+	postRecent=""
+	postArchive=""
+	for (i=0; i < posts.length; i++){
+		itemLink=relativePath + posts[i].file
+
+		itemTitle=getTitle(i,posts)
+		itemDate=getDate(i,posts)
+		itemDesc=getDesc(i,posts)
+		itemImg=getImg(i,posts)
+
+		item='\
+		<a href="' + itemLink + '">\
+		<div class="blog-item">\
+			<div class="item-title">' + itemTitle + '</div>\
+			<div class="item-date">' + itemDate + '</div>\
+			<div class="item-desc">' + itemDesc + '</div>\
+			<div class="item-img"><img src="' + itemImg +'"></div>\
+		</div>\
+		</a>\
+		'	
+		postArchive+=item
+		if(i<3){postRecent+=item}
+	}
+	if(e=document.querySelector("#blog-archive")){e.innerHTML=postArchive;}
+	if(e=document.querySelector("#blog-recent")){e.innerHTML=postRecent;}
 }
 
-function addToTag(tag,input) {
-    tag = document.getElementsByTagName(tag)[0]
-    addTo(tag,input)
-}
-function addToId(id,input) {
-    id = document.getElementById(id)
-    addTo(id,input)
-}
-function addToQuery(query,input) {
-    query = document.querySelector(query)
-    addTo(query,input)
+if(document.title===""){
+	document.title=blogname;
+}else{
+	document.title+= " | " + blogname;
 }
 
-function getIndex(z){
-	currentIndex = -1;
+//-----------------------------
+
+// [5] FUNCTION - what the funk!
+
+function getIndex(e){
 	currentFile = url.substring(url.lastIndexOf('/'));
-	currentFile = currentFile.replaceAll("/","")
+	currentFile = currentFile.replaceAll("/","");
 	if ( ! currentFile.endsWith(".html") ) {
 		currentFile += ".html";
 	}
 
-	for (i = 0; i < z.length; i++) {
-		if ( z[i].file === currentFile ) {
-			currentIndex = i;
-            break
+	for (i = 0; i < e.length; i++) {
+		if ( e[i].file === currentFile ) {
+			return i
 		}
 	}
+	return -1
 }
-function getTitle(i, z){
-        if ( z[i].alt !== null){
-            title = z[i].alt;
-        } else {
-            title = z[i].file;
-            title = title.slice(11, title.lenght);
-            title = title.replaceAll("-"," ");
-            title = title.replaceAll(".html","");
-        }
-	return title;
+function getTitle(i, e){
+    if ( e[i].hasOwnProperty("alt") ){
+        title = e[i].alt;
+    } else {
+        title = e[i].file;
+        title = title.slice(11, title.lenght);
+        title = title.replaceAll("-"," ");
+        title = title.replaceAll(".html","");
+    }
+return title;
 }
-function getDate(i, z){
-	date = z[i].file.slice(0,10);
-	return date
+
+function getDate(i, e){
+date = e[i].file.slice(0,10);
+return date
 }
+
 function convDate(i){
 	month = i.slice(5,7);
 	day = i.slice(8,10);
@@ -116,52 +218,47 @@ function convDate(i){
 			break;
 		}
 	date = month + " " + day + ", " + year;
+	date = date.replaceAll("00,","")
 	return date;
 }
 
-function genNav(z){
-	if ( z.length < 2) {
-		result = "<a href='./index.html'>« Archive  »</a>";
-	} else if ( currentIndex === 0 ) {
-		prevI= z[currentIndex+1].file;
-		result = "<a href='./index.html'>Archive</a> | <a href='./" + prevI + "'>Prev »</a>";
-	} else if ( currentIndex === z.length - 1 ) {
-		nextI= z[currentIndex-1].file;
-		result = "<a href='./" + nextI + "'>« Next</a> | <a href='./index.html'>Archive</a>";
-		
-	} else if ( 0 < currentIndex && currentIndex < z.length - 1 ) {
-		prevI= z[currentIndex+1].file;
-		nextI= z[currentIndex-1].file;
-		result = "<a href='./" + nextI + "'>« Next</a> | <a href='./index.html'>Archive</a> | <a href='./" + prevI + "'>Prev »</a>";
+function getDesc(i,e){
+	if (e[i].hasOwnProperty("desc")){
+		return e[i].desc;
 	}
+	return ''
 }
 
-getIndex(masterlist)
-    if ( currentIndex > -1) {
-        genNav(masterlist);
-        currentTitle = getTitle(currentIndex, masterlist);
-        currentDate = getDate(currentIndex, masterlist);
-        if (document.title === "") {
-            currentDate=currentDate.replaceAll("-00","")
-            document.title = currentDate + " | Stupied";
-        }
-        currentDate = convDate(currentDate);
-        addToId('blogTitle',currentTitle);
-        addToId('blogDate',currentDate);
-        addToId('nextprev',result);
-    }else{
-        addToId('blogTitle',"[Unlisted Post]");
-        addToId('nextprev',"<a href='./index.html'>« Archive  »</a>")
-        if (document.title === "") {document.title = "Unlisted Post | Stupied";}
-        list=""
-        for(i=0;i<masterlist.length;i++){
-            date = getDate(i, masterlist);
-            title = getTitle(i, masterlist);
-			link= masterlist[i].file
-			item='<li><a href="'+link+'"><span>'+date+" »</span> <span class='txt-darker-yellow'>"+title+'</span></a></li>'
-            list+=item
-        }
-		list="<ul>"+list+"</ul>"
-        addToId("blogList",list)
+function getImg(i,e){
+	if (e[i].hasOwnProperty("img")){
+		return relativePath + e[i].img;
+	}
+	return relativePath + ''
+}
+
+function genNav(e){
+	if ( e.length < 2) {
+		result = "<a href='" + relativePath + "archive.html'>« Archive  »</a>";
+	} else if ( currentIndex === 0 ) {
+		prevI= e[currentIndex+1].file;
+		result = "<a href='" + relativePath + "archive.html'>Archive</a> | <a href='./" + prevI + "'>Prev »</a>";
+	} else if ( currentIndex === e.length - 1 ) {
+		nextI= e[currentIndex-1].file;
+		result = "<a href='./" + nextI + "'>« Next</a> | <a href='" + relativePath + "archive.html'>Archive</a>";
+		
+	} else if ( 0 < currentIndex && currentIndex < e.length - 1 ) {
+		prevI= e[currentIndex+1].file;
+		nextI= e[currentIndex-1].file;
+		result = "<a href='./" + nextI + "'>« Next</a> | <a href='" + relativePath + "archive.html'>Archive</a> | <a href='./" + prevI + "'>Prev »</a>";
+	}
+	return result
+}
+
+//comments
+if (e=document.getElementById("comments")){e.innerHTML='<div id="HCB_comment_box"><a href="http://www.htmlcommentbox.com">Beep Boop</a>, hold please!</div><link rel="stylesheet" type="text/css" href="https://www.htmlcommentbox.com/static/skins/bootstrap/twitter-bootstrap.css?v=0" /><style>#HCB_comment_box img{width:auto;display:inline-block;}.home-desc{display:none;}#HCB_comment_box h3:first-child{margin:0;}</style>'}
+
+window.onload = function(){
+    if(document.getElementById("comments")){
+        if(!window.hcb_user){hcb_user={};} (function(){var s=document.createElement("script"), l=hcb_user.PAGE || (""+window.location).replace(/'/g,"%27"), h="https://www.htmlcommentbox.com";s.setAttribute("type","text/javascript");s.setAttribute("src", h+"/jread?page="+encodeURIComponent(l).replace("+","%2B")+"&mod=%241%24wq1rdBcg%24lorU9Glfj8bQyg9yk9caG%2F"+"&opts=16798&num=10&ts=1699153972795");if (typeof s!="undefined") document.getElementsByTagName("head")[0].appendChild(s);})();
     }
-    addToTag("nav",'<header>The Abyss</header><a href="'+relativePath+'blog/index.html" title="list of all the blog posts">Archive</a><a href="https://status.cafe/users/stupied" title="small random thoughts">Status</a><a href="'+relativePath+'index.html" title="head home">Surface ✮</a>')
+}
