03f2561a35c9dc8a9e655e20e0d70523345570b4
Stupiedidiot <anothernoobeyneko@gmail.com>
2024-12-06 22:09:19 +0800

    added lightbox for gallery :D

--- public/art/main.js
+++ public/art/main.js
@@ -2,29 +2,40 @@ function wrap(el, wrapper) {el.parentNode.insertBefore(wrapper, el);wrapper.appe
 
 //template stuff
 const navigation =
-'<a href="./../about.html">\
-  <img id="aside-1-img" src="./../meta/pfp-transparent.png">\
-</a>\
-<nav>\
-  <a href="./index.html">home</a>\
-  <a href="./archive.html">archive</a>\
-  <a href="#">code</a>\
-  <a href="#">commissions</a>\
-</nav>'
+`<a href="./../about.html">
+  <img id="aside-1-img" src="./../meta/pfp-transparent.png">
+</a>
+<nav>
+  <a href="./index.html">home</a>
+  <a href="./archive.html">archive</a>
+  <a href="#">code</a>
+  <a href="#">commissions</a>
+</nav>`
 
 const footer =
 '<a href="#">Sitemap</a>\
 <a href="./../index.html">Head Back</a>\
 <a href="./../chat.html">Chatbox</a>'
 
-const template =
-'<div id="container">\
-  <header></header>\
-  <div id="aside-1">'+navigation+'</div>\
-  <main></main>\
-  <div id="aside-2"></div>\
-  <footer>'+footer+'</footer>\
-</div>'
+const lightbox =`
+<div id="lightbox">
+  <button href="MEOW" onclick="closeLightBox()">X</button>
+  <div>
+    <img src="#">
+    <a id="lightbox-prev">« Prev</a> |
+    <a href="#" id="lightbox-details">View Details</a> |
+    <a id="lightbox-next">Next »</a>
+  </div>
+</div>
+`
+
+const template =lightbox + `
+<div id="container">
+  <header></header>
+  <div id="aside-1">`+navigation+`</div>
+  <main></main>
+  <footer>`+footer+`</footer>
+</div>`
 
 document.getElementsByTagName("body")[0].innerHTML+=template
 
@@ -34,27 +45,41 @@ for(i=0; i<main.length; i++){
 }
 
 if(e=document.getElementById("aside-1-content")){document.querySelector("#container #aside-1").append(e)}
-if(e=document.getElementById("aside-2-content")){document.querySelector("#container #aside-2").append(e)}
 
 if(document.title===""){document.title="gallery // stupied"}else{document.title+= " - gallery // stupied";}
 
-document.querySelector("head").innerHTML+='<link rel="icon" type="image/x-icon" href="./../meta/favicon.ico"></link>'
+document.querySelector("head").innerHTML+='<link rel="icon" type="image/x-icon" href="./../favicon.ico"></link>'
 
 //adding links to iamges
 images=document.querySelectorAll(".justified-gallery img")
 for(i=0;i<images.length;i++){
-  //get image links
+  linkHTML=document.createElement("a")
+  linkHTML.style.setProperty("--width",images[i].style.getPropertyValue("--width"))
+  linkHTML.style.setProperty("--height",images[i].style.getPropertyValue("--height"))  
+  linkHTML.setAttribute("onclick",'openImg('+i+')')
+  
+  wrap(images[i],linkHTML)
+}
+
+function openImg(i){  
+  document.querySelector("#lightbox img").src=images[i].src
+
+  document.getElementById("lightbox").style.height="100vh"
+
+  if(images[i-1]){prev=i-1}else{prev=images.length-1}
+  if(images[i+1]){next=i+1}else{next=0}
+  document.getElementById("lightbox-prev").setAttribute("onclick",'openImg('+prev+')')
+  document.getElementById("lightbox-next").setAttribute("onclick",'openImg('+next+')')
+
   link=images[i].src
   link=link.lastIndexOf('/', link.lastIndexOf('/')-1)+1
   link="./view.html?item="+images[i].src.slice(link,link.length)
   link=link.replaceAll("img/","")
+  document.getElementById("lightbox-details").href=link  
+}
 
-  //make n wrap anchor tag
-  linkHTML=document.createElement("a")
-  linkHTML.href=link
-  linkHTML.style.setProperty("--width",images[i].style.getPropertyValue("--width"))
-  linkHTML.style.setProperty("--height",images[i].style.getPropertyValue("--height"))
-  wrap(images[i],linkHTML)
+function closeLightBox(){
+  document.getElementById("lightbox").style.height=0
 }
 
 //comments
@@ -67,9 +92,9 @@ window.onload = function(){
 }
 
 // erm
-url=window.location.href
-if(url.includes("https://stupied.neocities.org/art/view")){
-  if(url.includes(".html")){
+urlHref=window.location.href
+if(urlHref.includes("https://stupied.neocities.org/")){
+  if(urlHref.includes(".html")){
     window.location.pathname=window.location.pathname.replaceAll(".html","")
-  }
+  }ur
 }
