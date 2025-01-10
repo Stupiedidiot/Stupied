b6b4c4d26688d70ef5eb57ad8bca86141af73974
Stupiedidiot <anothernoobeyneko@gmail.com>
2025-01-10 16:49:34 +0800

    updated psych index

---
+++ public/comic/psych/main.js
@@ -0,0 +1,23 @@
+const templateMain =`
+    <div id="container">
+        <main></main>
+        <aside>
+            <div id="aside-main">
+                <span class="title outline"><a href="#">PSYCH!</a></span>
+                <nav>
+                    <a href="#">/ARCHIVE/</a>
+                    <a href="./character.html">/CHARCTERS/</a>
+                    <a href="./../../index.html">✮</a>
+                </nav>
+            </div>
+            <img class="outline" src="./img/snail.png">
+        </aside>
+    </div>
+`
+
+document.querySelector("body").innerHTML+= templateMain;
+
+main=document.querySelectorAll(".main-content");
+for(i=0; i<main.length; i++){
+    document.querySelector("#container main").append(main[i]);
+}
